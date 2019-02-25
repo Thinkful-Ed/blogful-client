9223904fe3d23d8125c6d194e85c957f499a98d5
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2019-02-25 16:57:59 +0000

    adds POST request to /api/users in user registration form

--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Button, Input, Required } from '../Utils/Utils'
+import AuthApiService from '../../services/auth-api-service'
 
 export default class RegistrationForm extends Component {
   static defaultProps = {
@@ -12,14 +13,23 @@ export default class RegistrationForm extends Component {
     ev.preventDefault()
     const { full_name, nick_name, user_name, password } = ev.target
 
-    console.log('registration form submitted')
-    console.log({ full_name, nick_name, user_name, password })
-
-    full_name.value = ''
-    nick_name.value = ''
-    user_name.value = ''
-    password.value = ''
-    this.props.onRegistrationSuccess()
+    this.setState({ error: null })
+    AuthApiService.postUser({
+      user_name: user_name.value,
+      password: password.value,
+      full_name: full_name.value,
+      nickname: nick_name.value,
+    })
+      .then(user => {
+        full_name.value = ''
+        nick_name.value = ''
+        user_name.value = ''
+        password.value = ''
+        this.props.onRegistrationSuccess()
+      })
+      .catch(res => {
+        this.setState({ error: res.error })
+      })
   }
 
   render() {
--- src/services/auth-api-service.js
+++ src/services/auth-api-service.js
@@ -15,6 +15,20 @@ const AuthApiService = {
           : res.json()
       )
   },
+  postUser(user) {
+    return fetch(`${config.API_ENDPOINT}/users`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  },
 }
 
 export default AuthApiService
